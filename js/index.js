b97808c8c4d5130b9f124df1395dc270b6a9d2e3
Hudson Davi <hudson.davi.cds@gmail.com>
2022-03-17 21:10:26 -0400

    Edição na borda das abas do formulário

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 //Exibe a Aba de Login e Esconde a de Cadastro
 function abaLogin(){
+	var abasLogin = document.getElementById("formulario-aba-login");
+	var abasCadastro = document.getElementById("formulario-aba-cadastro");
     var login = document.getElementById("formulario-login");
     var cadastro = document.getElementById("formulario-cadastro");
+	abasLogin.style.borderBottom = "none";
+	abasCadastro.style.borderBottom = "1px solid #fff";
     login.style.display = "block";
     cadastro.style.display = "none";
 }
 //Exibe a Aba de Cadastro e Esconde a de Login
 function abaCadastro(){
+	var abasLogin = document.getElementById("formulario-aba-login");
+	var abasCadastro = document.getElementById("formulario-aba-cadastro");
     var login = document.getElementById("formulario-login");
     var cadastro = document.getElementById("formulario-cadastro");
+	abasLogin.style.borderBottom = "1px solid #fff";
+	abasCadastro.style.borderBottom = "none";
     login.style.display = "none";
     cadastro.style.display = "block";
 }
